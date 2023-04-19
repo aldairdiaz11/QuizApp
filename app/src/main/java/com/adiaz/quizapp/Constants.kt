1ef591e539126ae7861e14aabf43f233df575923
aldairdiaz <irving.s21diaz@gmail.com>
2023-04-18 20:19:13 -0600

    Quiz activity UI and data

--- app/src/main/java/com/adiaz/quizapp/Constants.kt
+++ app/src/main/java/com/adiaz/quizapp/Constants.kt
@@ -2,14 +2,30 @@ package com.adiaz.quizapp
 
 object Constants {
     fun getQuestions(): ArrayList<Question> {
-        val questionsList = ArrayList<Question>()
 
-        val que1 = Question(
-            1, "What does this flag belong to?", R.drawable.ic_flag_of_argentina,
-            "Brazil", "Germany", "Argentina", 3
+        // Questions data:
+        val questionStatement: String = "What does this flag belong to?"
+
+        val flags = arrayOf<Int>(
+            R.drawable.ic_flag_of_argentina, R.drawable.ic_flag_of_australia,
+            R.drawable.ic_flag_of_belgium, R.drawable.ic_flag_of_brazil, R.drawable.ic_flag_of_denmark,
+            R.drawable.ic_flag_of_fiji, R.drawable.ic_flag_of_germany, R.drawable.ic_flag_of_india,
+            R.drawable.ic_flag_of_kuwait, R.drawable.ic_flag_of_new_zealand)
+
+        val answers = arrayOf<String>(
+            "Argentina", ""
         )
 
-        questionsList.add(que1)
+        val questionsList = ArrayList<Question>()
+
+        for ((index, flag) in flags.withIndex()) {
+            questionsList.add(
+                Question(
+                    1, questionStatement, flag,
+                    "Brazil", "Germany", "Argentina", 3
+                )
+            )
+        }
 
         return questionsList
     }
