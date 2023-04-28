f03527393450128141d1c601e95a2b9431cb9bf0
aldairdiaz <irving.s21diaz@gmail.com>
2023-04-27 21:35:16 -0600

    Update data and Options views

--- app/src/main/java/com/adiaz/quizapp/Constants.kt
+++ app/src/main/java/com/adiaz/quizapp/Constants.kt
@@ -4,26 +4,43 @@ object Constants {
     fun getQuestions(): ArrayList<Question> {
 
         // Questions data:
-        val questionStatement: String = "What does this flag belong to?"
+        val questionStatement = "What does this flag belong to?"
 
-        val flags = arrayOf<Int>(
+        val flags = arrayOf(
             R.drawable.ic_flag_of_argentina, R.drawable.ic_flag_of_australia,
             R.drawable.ic_flag_of_belgium, R.drawable.ic_flag_of_brazil, R.drawable.ic_flag_of_denmark,
             R.drawable.ic_flag_of_fiji, R.drawable.ic_flag_of_germany, R.drawable.ic_flag_of_india,
             R.drawable.ic_flag_of_kuwait, R.drawable.ic_flag_of_new_zealand)
 
-        val answers = arrayOf<String>(
-            "Argentina", ""
+        val answers = arrayOf(
+            0, 1, 2, 3, 1, 2, 3, 1, 2, 3
+        )
+
+        val options = arrayOf(
+            arrayOf("Argentina", "New Zealand", "Kuwait", "Peru"),
+            arrayOf("New Zealand", "Australia", "Denmark", "Portugal"),
+            arrayOf("Brazil", "New Zealand", "Belgium", "Russia"),
+            arrayOf("Argentina", "Mexico", "Ukrainian", "Brazil"),
+            arrayOf("Australia", "Denmark", "Germany", "Uruguay"),
+            arrayOf("Canada", "Colombia", "Fiji", "Venezuela"),
+            arrayOf("China", "Japan", "South Korea", "Germany"),
+            arrayOf("Chile", "India", "Italy", "Finland"),
+            arrayOf("Cuba", "Spain", "Kuwait", "Singapore"),
+            arrayOf("Kenya", "France", "Azerbaijan","New Zealand")
         )
 
         val questionsList = ArrayList<Question>()
 
         for ((index, flag) in flags.withIndex()) {
+            val optOne = options[index][0]
+            val optTwo = options[index][1]
+            val optThree = options[index][2]
+            val optFour = options[index][3]
+
             questionsList.add(
                 Question(
-                    1, questionStatement, flag,
-                    "Brazil", "Germany", "Argentina",
-                    "India", 3
+                    index + 1, questionStatement, flag, optOne, optTwo, optThree, optFour,
+                    answers[index]
                 )
             )
         }
--- app/src/main/java/com/adiaz/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/adiaz/quizapp/QuizQuestionsActivity.kt
@@ -1,14 +1,22 @@
 package com.adiaz.quizapp
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var absCurrentPosition: Int = 1
+    private var absQuestionsList: ArrayList<Question>? = null
+    private var absSelectedOptPos: Int = 0
 
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
@@ -20,6 +28,8 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private var tvOptionThree: TextView? = null
     private var tvOptionFour: TextView? = null
 
+    private var btnSubmit: Button? = null
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,28 +40,111 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvQuestion = findViewById(R.id.tv_question)
         tvImage = findViewById(R.id.tv_image)
 
-        tvOptionOne =findViewById(R.id.tv_option_one)
-        tvOptionTwo =findViewById(R.id.tv_option_two)
-        tvOptionThree =findViewById(R.id.tv_option_three)
-        tvOptionFour =findViewById(R.id.tv_option_four)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Question list size: ", "${questionsList.size}")
+        btnSubmit = findViewById(R.id.btn_submit)
+        absQuestionsList = Constants.getQuestions()
 
-        for (item in questionsList) {
-            Log.e("Question", item.question)
-        }
+        // Make tv clickable
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+        btnSubmit?.setOnClickListener(this)
 
-        var currentPosition = 1
-        val question: Question = questionsList[currentPosition - 1]
+        setQuestion()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setQuestion() {
+
+        val question: Question = absQuestionsList!![absCurrentPosition - 1]
         tvImage?.setImageResource(question.image)
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        progressBar?.progress = absCurrentPosition
+        tvProgress?.text = "$absCurrentPosition / ${progressBar?.max}"
         tvQuestion?.text = question.question
 
         tvOptionOne?.text = question.OptionOne
         tvOptionTwo?.text = question.OptionTwo
         tvOptionThree?.text = question.OptionThree
         tvOptionFour?.text = question.OptionFour
+
+        if (absCurrentPosition == absQuestionsList!!.size) {
+            btnSubmit?.text = "FINISH"
+        }
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, optNum: Int) {
+        defaultOptionsView()
+        absSelectedOptPos = optNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.tv_option_one -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+
+                }
+            }
+
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+
+            R.id.tv_option_three -> {
+                tvOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+
+            R.id.tv_option_four -> {
+                tvOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+            }
+
+            R.id.btn_submit -> {
+                // todo "implement btn submit"
+            }
+        }
     }
 }
